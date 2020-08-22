ddf7d6c08ffdae54fbda83aa207e4273ecbf5643
NITRO 5 <Peerapatpee@hotmail.com>
2020-08-22 18:12:46 +0700

    FirstAppAgain:Update MainActivity.kt and activity_main.xml

--- app/src/main/java/com/example/firstappagain/MainActivity.kt
+++ app/src/main/java/com/example/firstappagain/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.firstappagain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.firstappagain.databinding.ActivityMainBinding
+import com.google.android.material.internal.NavigationMenuView
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigation_view: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        navigation_view = binding.navView
+
 
     }
 }
