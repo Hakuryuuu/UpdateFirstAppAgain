f1128ee7ef62750a04899e4fe659318d90a2b13a
NITRO 5 <Peerapatpee@hotmail.com>
2020-08-25 21:06:44 +0700

    FirstAppAgain:create Nav for addlist and add

--- app/src/main/java/com/example/firstappagain/AddContactFragment.kt
+++ app/src/main/java/com/example/firstappagain/AddContactFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_addcontact.view.*
 
 class AddContactFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +14,14 @@ class AddContactFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_addcontact,container,false)
+        var view = inflater.inflate(R.layout.fragment_addcontact,container,false)
+
+        view.floatingActionButton.setOnClickListener{
+            Navigation.findNavController(view!!).navigate(R.id.action_addContactFragment_to_addFragment)
+        }
+
+
+        return view
     }
 }
 
